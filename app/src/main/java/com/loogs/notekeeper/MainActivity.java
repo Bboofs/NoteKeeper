03b8afcf6d7aa38e70853a73c4646480be55f665
McOluga <bboofs@yahoo.com>
2019-08-10 16:51:56 +0300

    COMPLETED the Course: Enhancing the Android Application Experience

--- app/src/main/java/com/loogs/notekeeper/MainActivity.java
+++ app/src/main/java/com/loogs/notekeeper/MainActivity.java
@@ -1,6 +1,7 @@
 package com.loogs.notekeeper;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -26,6 +27,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.Menu;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -69,6 +71,22 @@ public class MainActivity extends AppCompatActivity
         super.onResume();
         //mAdapterNotes.notifyDataSetChanged();
         mNoteRecyclerAdaptor.notifyDataSetChanged();
+
+        updateNavHeader();
+    }
+
+    private void updateNavHeader() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView textUserName = (TextView) headerView.findViewById(R.id.text_user_name);
+        TextView textEmailAddress = (TextView) headerView.findViewById(R.id.text_email_address);
+
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        String userName = pref.getString("user_display_name", "");
+        String emailAddress = pref.getString("user_email_address", "");
+
+        textUserName.setText(userName);
+        textEmailAddress.setText(emailAddress);
     }
 
     private void initializeDisplayContent() {
@@ -150,7 +168,8 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_courses) {
             displayCourses();
         } else if (id == R.id.nav_share) {
-            handleSelection(R.string.nav_share_message);
+            //handleSelection(R.string.nav_share_message);
+            handleShare();
 
         } else if (id == R.id.nav_send) {
             handleSelection(R.string.nav_send_message);
@@ -159,6 +178,13 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void handleShare() {
+        View view = findViewById(R.id.list_items);
+        Snackbar.make(view, "Share to - " +
+                PreferenceManager.getDefaultSharedPreferences(this)
+                        .getString("user_favourite_social", ""), Snackbar.LENGTH_LONG).show();
     }
 
     private void handleSelection(int message_id) {
